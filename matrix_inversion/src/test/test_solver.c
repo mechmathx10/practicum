d742cf8f602d7d331b17b8a6aadc105a59278645
Vladimir Tyrin <b0-0b@yandex.ru>
2014-10-21 18:03:10 +0400

    matrix_inversion : test_solver template added

---
+++ matrix_inversion/src/test/test_solver.c
@@ -0,0 +1,13 @@
+#include "test_solver.h"
+
+/* ----------------------------------------------------------- */
+
+enum error_type
+inverse_matrix(struct simple_matrix *matrix, struct simple_matrix *result)
+{
+  UNUSED(matrix);
+  UNUSED(result);
+  return ET_CORRECT;
+}
+
+/* ----------------------------------------------------------- */
---
+++ matrix_inversion/src/test/test_solver.h
@@ -0,0 +1,13 @@
+#ifndef TEST_SOLVER_H
+#define TEST_SOLVER_H
+
+#include "../datatypes.h"
+
+/* ----------------------------------------------------------- */
+
+enum error_type
+inverse_matrix(struct simple_matrix *matrix, struct simple_matrix *result);
+
+/* ----------------------------------------------------------- */
+
+#endif // TEST_SOLVER_H
